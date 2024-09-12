df1eef8da98eaee66de1d62422aadf29ed05a00e
Viclegrand28 <viclegranddab@gmail.com>
2024-09-12 21:32:53 +0100

    changes on 12 09 2024 - 2

--- scripting/stop.sh
+++ scripting/stop.sh
@@ -12,14 +12,14 @@ param2=$2
 # Trouver et arrêter tous les processus en cours
 echo "Arrêt des processus en cours..."
 for alpha in "${alphas[@]}"; do
-    SCREEN_NAME="${param1}_GT_"
+    SCREEN_NAME="${param1}_GT_${alpha:1}"
     pids=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | tr -d '.')
     if [ -n "$pids" ]; then
         echo "Arrêt des processus pour '$SCREEN_NAME'"
         screen -S "$SCREEN_NAME" -X quit
     fi
 
-    SCREEN_NAME="${param1}_GF_"
+    SCREEN_NAME="${param1}_GF_${alpha:1}"
     pids=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | tr -d '.')
     if [ -n "$pids" ]; then
         echo "Arrêt des processus pour '$SCREEN_NAME'"
